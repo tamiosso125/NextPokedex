d705d00497d59186e76ff3a9628d4da87ea49f92
Guilherme Ribeiro Lemos Tamiosso <guilherme.tamiosso@hotmail.com>
2023-03-14 15:27:55 -0500

    All pokemons

--- src/pages/pokemon/[pokemonId].js
+++ src/pages/pokemon/[pokemonId].js
@@ -1,6 +1,7 @@
 import styles from '../../styles/Pokemon.module.css'
 
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 export const getStaticPaths = async () => {
   const maxPokemons = 251
@@ -18,7 +19,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   }
 }
 
@@ -35,6 +36,12 @@ export const getStaticProps = async (context) => {
 }
 
 export default function Pokemon({ pokemon }) {
+
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <div>Carregando</div>
+  }
   return (
     <div className={styles.pokemon_container}>
       <h1 className={styles.title}>{pokemon.name}</h1>
